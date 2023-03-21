1b5aeb0adc45d0d1ff20fc832a7387f4f257c594
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-03-21 15:53:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-	for(let i = 0; i < str.length; i++){
+	str = str.toLowerCase();
+	for(var i = 0; i < str.length; i++){
 		if(str[i] !== str[str.length - 1 - i]) {
 			return false;
 		}
